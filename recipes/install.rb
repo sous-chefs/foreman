bab94689c35a860a6f29b6131bb8b77902a83c8a
gm <gunter.miegel@rgsqd.de>
2017-02-09 20:19:36 +0100

    Take care that Ruby 2.0.0 is working on Trusty.

--- recipes/install.rb
+++ recipes/install.rb
@@ -5,6 +5,26 @@
 #
 include_recipe 'foreman::repo' if node['foreman']['use_repo']
 
+# Ubuntu Trusty (14.04) comes with Ruby 1.9.1 as default Ruby version.
+# Foreman 1.14 requires Ruby >= 2.0.0.
+package 'ruby2.0'
+
+# Setting 'ruby2.0' as alternative Ruby version is broken in Trusty.
+# https://bugs.launchpad.net/ubuntu/+source/ruby2.0/+bug/1310292
+# So we have to adjust the symlinks on our own.
+# TODO Move these commands in to Chef ressources.
+bash 'Handle to broken Ruby 2.0 in Trusty.' do
+  code <<-EOH
+  rm /usr/bin/ruby /usr/bin/gem /usr/bin/irb /usr/bin/rdoc /usr/bin/erb
+  ln -s /usr/bin/ruby2.0 /usr/bin/ruby
+  ln -s /usr/bin/gem2.0 /usr/bin/gem
+  ln -s /usr/bin/irb2.0 /usr/bin/irb
+  ln -s /usr/bin/rdoc2.0 /usr/bin/rdoc
+  ln -s /usr/bin/erb2.0 /usr/bin/erb
+  EOH
+  only_if 'ruby -v | grep -q ruby\ 1.9'
+end
+
 package 'foreman' do
   version node['foreman']['version'] unless
     node['foreman']['version'] == 'stable'
